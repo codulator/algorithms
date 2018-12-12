1951a81d97724f714b715bcc8b1ce7cc0df30110
Puja K <pumpkin@Pujas-MacBook-Air.local>
2018-12-11 19:29:11 -0800

    Check if edit distance between 2 strings is 1

---
+++ algos/OneAway.java
@@ -0,0 +1,45 @@
+package algos;
+
+public class OneAway {
+
+    private boolean isOneEditAway(String str1, String str2) {
+        if (str1 == null || str2 == null)
+            return false;
+
+        int m = str1.length();
+        int n = str2.length();
+
+        int diff = 0;
+
+        int i,j;
+        for (i =0, j = 0; i < m && j < n;) {
+            if (str1.charAt(i) != str2.charAt(j)){
+                if (diff == 1)
+                    return false;
+
+                if (m > n) {
+                    i++;
+                } else if (n > m) {
+                    j++;
+                } else {
+                    i++;
+                    j++;
+                }
+                diff++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+        if (i < m || j < n)
+            diff++;
+        return diff == 1;
+    }
+
+    public static void main(String[] args) {
+        OneAway oneAway = new OneAway();
+        System.out.println("Are strings one edit away ? " + oneAway.isOneEditAway("pale", "ple"));
+        System.out.println("Are strings one edit away ? " + oneAway.isOneEditAway("bale", "pale"));
+        System.out.println("Are strings one edit away ? " + oneAway.isOneEditAway("kale", "pade"));
+    }
+}
